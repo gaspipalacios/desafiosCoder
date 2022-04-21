329b02db069591f420b171cbf357d0a534bcaaf4
Gaspar <gpalaciostorre@gmail.com>
2022-04-21 19:34:25 -0300

    Correcciones desafio clase8

--- desafios/clase8/index.js
+++ desafios/clase8/index.js
@@ -5,9 +5,14 @@ const app = express()
 const productsRoute = require("./routes/products.js")
 
 //Para poder leer req.body
+app.use(express.urlencoded())
 app.use(express.json())
 
 app.use("/api/products", productsRoute)
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
